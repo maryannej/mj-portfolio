66b98b76318ad132bee8a7c3be6de4548cefa052
maryannej <79432046+maryannej@users.noreply.github.com>
2022-06-28 09:10:57 +1000

    commit

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,10 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
