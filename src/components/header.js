04b1a295601f424587aac436f03364cc99c8af8e
maryannej <79432046+maryannej@users.noreply.github.com>
2022-06-28 09:27:33 +1000

    font

--- src/components/header.js
+++ src/components/header.js
@@ -27,11 +27,6 @@ const Header = ({ siteTitle }) => (
         >
           {siteTitle}
 
-          
-        </Link>
-        link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
       </h1>
     </div>
 
